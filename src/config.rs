579f085c2762f13a7085b967227b20110564281b
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2025-07-13 14:55:15 +0800

    Add comments to describe the config constants

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,10 @@
 /// Application configuration constants
-pub const INITIAL_SECONDS: u32 = 25 * 60;
+
+/// 25:00 in seconds for the initial countdown
+pub const INITIAL_SECONDS: i32 = 25 * 60;
+
+/// 1 second in milliseconds for countdown updates
 pub const COUNTDOWN_INTERVAL_MS: u32 = 1000;
+
+/// 200 milliseconds in milliseconds for blink toggling
 pub const BLINK_INTERVAL_MS: u32 = 200;
